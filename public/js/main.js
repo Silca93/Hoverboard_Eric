c59bed05561c923becfb586b0cca2ad5a18a27d5
Eric_PC_FIXE <edelhaye93@gmail.com>
2024-01-21 15:33:00 +0100

    modification in transition

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,13 @@
 let onHover = document.querySelectorAll(".horizontal")
-
-let colorArray = ["red", "orange", "blue", "yellow", "purple", "green"]
-
-
+let colorArray = ["red", "orangered", "dodgerblue", "purple", "forestgreen"]
 onHover.forEach((element) => {
      element.addEventListener('mouseover', () => {
         let randomColor = colorArray[Math.floor(Math.random()*colorArray.length)]
         element.style.backgroundColor = randomColor
     })
+    element.addEventListener('mouseout', () => {
+        setTimeout(() => {
+        element.style.backgroundColor = "transparent";
+        }, 500);
+})
 })
